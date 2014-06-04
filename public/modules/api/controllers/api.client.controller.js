26dfc3bfe77d1e6335a652c6429e7831229b9a99
tmh144429 <tmh144429@gmail.com>
2014-06-04 10:59:55 -0400

    add all

--- public/modules/api/controllers/api.client.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('api').controller('FirstCtrl', ['$scope', 'Data',
-	function($scope, Data) {
-		$scope.data = Data;
-	};
-]);
--- public/modules/api/services/api.client.service.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-//
-angular.module('api').factory('Data', function() {
-    return {message: "I'm data from a Service"}
-});
-
