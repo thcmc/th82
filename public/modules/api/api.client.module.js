d8666e88414420a22d7270c20d026afcabe516f7
tmh144429 <tmh144429@gmail.com>
2014-06-04 10:08:17 -0400

    comment out api module

--- public/modules/api/api.client.module.js
+++ public/modules/api/api.client.module.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 // Use Application configuration module to register a new module
-ApplicationConfiguration.registerModule('api');
+ApplicationConfiguration.registerModule('api');*/
